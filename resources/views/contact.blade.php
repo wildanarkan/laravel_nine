03b5a1a746cb66a49ce28c5e55409381d25dee75
wildanarkan <cillmystic@gmail.com>
2024-03-29 18:02:40 +0700

    route

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Ini adalah halaman contact</h2>
+    <h2>{{ $name }}</h2>
+    <h2>{{ $instagram }}</h2>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return 'AHMAD WILDAN ARKAN';
+});
+
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
+
+Route::view('/contact', 'contact', ['name' => 'Ahmad Wildan Arkan', 'instagram' => '@wildandev']);
+
+Route::redirect('/contact', '/kontak');
+
+Route::get('/product/{id}', function ($id) {
+    return view('product', ['id' => $id]);
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/profile', function () {
+        return 'profil admin';
+    });
+    Route::get('/about', function () {
+        return 'about admin';
+    });
+    Route::get('/contact', function () {
+        return 'contact admin';
+    });
+    Route::get('/gallery', function () {
+        return 'gallery admin';
+    });
+});
